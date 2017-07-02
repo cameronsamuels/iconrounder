a0a00e7bca36fa5be5eaec7d1f0d161418506b65
CameronSamuels <satchmosanders@gmail.com>
2017-07-02 12:49:18 -0500

    Cleaned up script

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var $ = function(e){return document.querySelector(e)}, i = new Image(), s = new Image(), b = $('canvas'), c = b.getContext('2d');
 function l(e) {
     i.onload = function() {
-      var w = Math.min(this.width, this.height), h = Math.min(this.width, this.height);
-      b.width = w, b.height = h, s.width = w, s.height = h;
+      var w = Math.min(this.width, this.height);
+      b.width = w, b.height = w, s.width = w, s.height = w;
       s.onload = function() {
         c.drawImage(s, 0, 0);
 		    c.globalCompositeOperation = 'source-atop';
