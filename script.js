e70ceafc8d7a15334ada12e07a9a803efdf883c9
CameronSamuels <satchmosanders@gmail.com>
2017-07-02 12:27:26 -0500

    Cleaned up script

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
-var w = 0, h = 0, $ = function(e){return document.querySelector(e)}, i;
+var $ = function(e){return document.querySelector(e)}, i = new Image(), s = new Image(), b = $('canvas'), c = b.getContext('2d');
 function l(e) {
-    i = new Image();
     i.onload = function() {
-      w = this.width, h = this.height;
-      $('canvas').width = w, $('canvas').height = h,
-      s = new Image();
-      s.width = w, s.height = h;
-  	 	var b = $('canvas'), c = b.getContext('2d');
+      var w = this.width, h = this.height;
+      b.width = w, b.height = h, s.width = w, s.height = h;
       s.onload = function() {
         c.drawImage(s, 0, 0, w, h);
 		    c.globalCompositeOperation = 'source-atop';
 		    c.drawImage(i, 0, 0);
-        b.style.visibility = 'visible';
-        $('a').style.display = "block";
-    	  $('a').href=b.toDataURL("image/png");
+        b.style.visibility = 'visible', $('a').style.display = "block", $('a').href=b.toDataURL("image/png");
       }
       s.src = "mask.svg";
     }, i.src = e.target.result;
