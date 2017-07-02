f12bf82cea82c0d2618a886326142a5589ffe714
CameronSamuels <satchmosanders@gmail.com>
2017-07-02 11:12:46 -0500

    Removed comments

--- script.js
+++ script.js
@@ -3,12 +3,9 @@ function l(e) {
     i = new Image();
     i.onload = function() {
       w = this.width, h = this.height;
-      //Set canvas and svg mask to the correct dimensions
       $('canvas').width = w, $('canvas').height = h,
       s = new Image();
       s.width = w, s.height = h;
-      //Put the image data in the image views
-	    //Draw image and mask in canvas
   	 	var b = $('canvas'), c = b.getContext('2d');
       s.onload = function() {
         c.drawImage(s, 0, 0, w, h);
