6029ac152d818056aebbd31e9a36bcc46d3d182f
CameronSamuels <satchmosanders@gmail.com>
2017-07-02 12:45:10 -0500

    Made non square icons generate square exports

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 var $ = function(e){return document.querySelector(e)}, i = new Image(), s = new Image(), b = $('canvas'), c = b.getContext('2d');
 function l(e) {
     i.onload = function() {
-      var w = this.width, h = this.height;
+      var w = Math.min(this.width, this.height), h = Math.min(this.width, this.height);
       b.width = w, b.height = h, s.width = w, s.height = h;
       s.onload = function() {
-        c.drawImage(s, 0, 0, w, h);
+        c.drawImage(s, 0, 0);
 		    c.globalCompositeOperation = 'source-atop';
 		    c.drawImage(i, 0, 0);
         b.style.visibility = 'visible', $('a').href=b.toDataURL();
