20190d116b51637cc96ff7d8b9803daaef0fcde3
CameronSamuels <satchmosanders@gmail.com>
2017-07-01 21:03:16 -0500

    Cleaned up js

--- script.js
+++ script.js
@@ -3,24 +3,20 @@ var w = 0, h = 0, $ = function(e){return document.querySelector(e)}, i;
 function l(e) {
     i = new Image();
     i.onload = function() {
-        w = this.width, h = this.height;
-
-        //Set canvas and svg mask to the correct dimensions
-        $('#iconCanvas').width = w, $('#iconCanvas').height = h,
-        $('#svg').width = w, $('#svg').height = h;
-
-        //Put the image data in the image views
-        $('#icon').setAttribute('src', e.target.result);
+      w = this.width, h = this.height;
+      //Set canvas and svg mask to the correct dimensions
+      $('#iconCanvas').width = w, $('#iconCanvas').height = h,
+      $('#svg').width = w, $('#svg').height = h;
+      //Put the image data in the image views
+      $('#icon').setAttribute('src', e.target.result);
 	    $('#realIcon').setAttribute('src', e.target.result);
 	    $('#icon').style.visibility = 'visible';
       $('a').style.display = "block";
-
 	    //Draw image and mask in canvas
-	 	var b = $('canvas'), c = b.getContext('2d');
-		c.drawImage($('#svg'), 0, 0, w, h);
-		c.globalCompositeOperation = 'source-atop';
-		c.drawImage($('#realIcon'), 0, 0);
-
+  	 	var b = $('canvas'), c = b.getContext('2d');
+      c.drawImage($('#svg'), 0, 0, w, h);
+		  c.globalCompositeOperation = 'source-atop';
+		  c.drawImage($('#realIcon'), 0, 0);
     	$('a').href=b.toDataURL("image/png");
     }, i.src = e.target.result;
 };
