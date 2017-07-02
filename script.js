2db4116fb379fc8264799bade9b06e7d1ff3f696
CameronSamuels <satchmosanders@gmail.com>
2017-07-02 13:00:51 -0500

    Cleaned up script

--- script.js
+++ script.js
@@ -3,14 +3,11 @@ s.src = "s.svg";
 function l(e) {
     i.onload = function() {
       var w = Math.min(this.width, this.height);
-      b.width = w, b.height = w, s.width = w, s.height = w;
-      // s.onload = function() {
-        c.drawImage(s, 0, 0);
-		    c.globalCompositeOperation = 'source-atop';
-		    c.drawImage(i, 0, -(i.height-b.height)/2);
-        b.style.visibility = 'visible', $('a').href=b.toDataURL();
-      // }
-      // s.src = "s.svg";
+      b.width = w, b.height = w;
+      c.drawImage(s, 0, 0, w, w);
+      c.globalCompositeOperation = 'source-atop';
+      c.drawImage(i, 0, -(i.height-b.height)/2);
+      b.style.visibility = 'visible', $('a').href=b.toDataURL();
     }, i.src = e.target.result;
 }
 $('input').onchange = function() {
