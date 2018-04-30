f3a1872765a9b91f73569cf97b9bdf0a54237149
Cameron Samuels <me@cameronsamuels.com>
2018-04-29 20:02:57 -0500

    (update)Updated, renamed, and moved script

---
+++ script.js
@@ -0,0 +1,21 @@
+var template = new Image("template.svg");
+var icon = new Image();
+var canvas = document.querySelector("canvas");
+var context = canvas.getContext("2d");
+
+document.querySelector("input").addEventListener("change", function() {
+  if (!this.files) return;
+  var fileReader = new FileReader();
+  fileReader.addEventListener("load", function() {
+    icon.addEventListener("load", function() {
+      canvas.width = Math.min(icon.width, icon.height);
+      canvas.height = Math.min(icon.width, icon.height);
+      context.drawImage(template, 0, 0, canvas.width, canvas.height);
+      context.globalCompositeOperation = "source-atop";
+      context.drawImage(icon, (canvas.width - icon.width) / 2, (canvas.heigh - icon.height) / 2);
+      document.querySelector("a").href = canvas.toDataURL();
+    });
+    icon.src = fileReader.target.result; 
+  });
+  fileReader.readAsDataURL(this.files[0]);
+});
